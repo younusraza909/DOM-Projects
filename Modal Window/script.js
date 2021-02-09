787009500f1bea3434c813890f8d4a86c7bb3655
younusraza909 <younusraza909@gmail.com>
2021-02-09 08:23:28 +0500

    Working with classes

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,4 +1,26 @@
+//Selecting all necessary classes for DOM
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
+
+//Function to Close Modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+//Function  to open modal
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+//Iterating for adding eventListner
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", openModal);
+
+// Closing Modal
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
