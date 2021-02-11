58642ac16cdbd487d45761c95259dad95ec4aa20
younusraza909 <younusraza909@gmail.com>
2021-02-11 08:24:15 +0500

    Create ,Remove and delete element from dom

--- Bankist/script.js
+++ Bankist/script.js
@@ -8,7 +8,9 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
+const openModal = function (e) {
+  //to prevent default behaviour link
+  e.preventDefault();
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -18,8 +20,7 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
@@ -29,3 +30,23 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+//Creating and insterting cookies message to Page
+const header = document.querySelector(".header");
+
+//Creating element
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+
+message.innerHTML = `We use cookiees for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`;
+
+header.prepend(message);
+//dom element is unique so message can  be shown at one place only but if you want to make multiple of it you can use its clone
+// header.append(message.cloneNode(true));
+
+//Deleting Element
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
